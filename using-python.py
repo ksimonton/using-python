1c8f1022d6cb2ef07e8d0ecb686ee1ca744b1db3
ksimonton <katelynsimonton12@gmail.com>
2018-01-29 14:14:41 -0700

    Added python excerise file

---
+++ using-python.py
@@ -0,0 +1,15 @@
+# Katelyn Simonton - Using Python
+print("How are you?")
+
+#python numbers
+print(7 + 7.5)
+
+#python text
+print("Katelyn" * 5)
+print("Ha" * 2 + "Ho" * 2)
+print(*\n**\n***\n)
+print(*\n***\n*****\n***\n*****\n***\n*****\n*\n*\n)
+
+#python variables
+name = raw_input("What is your name? ")
+print("----------\n ~~~ + name + ~~~~ + ----------\n")
