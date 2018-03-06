619e763d5e22d5fe8e4b5b52b68e06329b8e94f4
ksimonton <33330459+ksimonton@users.noreply.github.com>
2018-03-06 13:38:22 -0700

    Add files via upload

---
+++ Using Python (updated verison).py
@@ -0,0 +1,16 @@
+# Katelyn Simonton - Using Python 
+
+#Running Python
+print("How are you?")
+
+#python numbers
+print(7 + 7.5)
+
+#python text
+print("Katelyn" * 5)
+print("Ha" * 2 + "Ho" * 2)
+print("**\n***\n****\n")
+
+#python variables
+name = input("What is your name? ")
+print("  ------------\n  ~~~ " + str(name) + " ~~~\n  ------------")
